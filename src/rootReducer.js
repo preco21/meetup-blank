0d12d69ef2b7fbad7785c6ee971cb94beaca3414
Plusb Preco <plusb21@gmail.com>
2016-11-25 11:11:00 +0900

    Remove obsoleted code

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,12 +1,10 @@
 import {combineReducers} from 'redux';
 import header from './components/Header/headerReducer';
 import content from './components/Content/contentReducer';
-// import sideBar from './components/SideBar/sideb';
 
 const rootReducer = combineReducers({
   header,
   content,
-  // sideBar,
 });
 
 export {
